8772c229d7439f19de98e558665bd2640c2b0621
Sobhit K <sobhitk@ecomexpress.in>
2023-06-28 16:16:30 +0530

    add thread pool

--- multitask.js
+++ multitask.js
@@ -1,8 +1,10 @@
-
-
 const http = require('http');
 const crypto = require("crypto");
 const fs = require('fs');
+/**
+ * By default node uses 4 threads in thread pool
+ */
+process.env.UV_THREADPOOL_SIZE = 5;
 
 const start = Date.now();
 
@@ -15,9 +17,9 @@ function doRequest() {
     }).end();
 }
 
-function doHash() {
+function doHash(serial_no) {
     crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
-        console.log("Hash: ", Date.now() - start);
+        console.log("Hash : " + serial_no, Date.now() - start);
     })
 }
 
@@ -27,10 +29,10 @@ fs.readFile('multitask.js', 'utf8', () => {
     console.log("FS: ", Date.now() - start);
 })
 
-doHash();
-doHash();
-doHash();
-doHash();
+doHash(1);
+doHash(2);
+doHash(3);
+doHash(4);
 
 
 
