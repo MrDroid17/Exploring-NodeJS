28e5467562989e47851cda9ef6b229073783b031
Sobhit Kumar <sobhit2010@gmail.com>
2023-07-17 17:56:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,14 @@
+const express = require('express');
+const app = express();
 
-const cluster = require('cluster');
-/**
- * check if process is master
- */
-if(cluster.isMaster){
-    /***
-     * create and manage child processes
-     */
-    cluster.fork();// 1st child process
-    // cluster.fork();// 2nd child process
-    // cluster.fork(); // 3rd child process
-    // cluster.fork(); // 4th child process
-} else {
-    
-    const express = require('express');
-    const app = express();
-    
-    function doWork(duration){
-        const start = Date.now();
-        while(Date.now()-start < duration){};
-    }
-    
-    
-    app.get("/", (req, res) => {
-        doWork(5000);
-        res.send("Hi There");
-    });
 
-    app.get("/fast", (req, res) => {
-        res.send("That was Fast.");
-    });
-    app.listen(3000);
-}
+app.get("/", (req, res) => {
+    crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+        res.send("Hi there");
+    })
+});
+
+app.get("/fast", (req, res) => {
+    res.send("That was Fast.");
+});
+app.listen(3000);
