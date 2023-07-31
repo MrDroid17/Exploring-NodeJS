0e0510a9acfce990a32efdd421698c1091d5ba71
Sobhit K <sobhitk@ecomexpress.in>
2023-07-31 17:58:27 +0530

    integrating redis

--- 02_AdvancedNodeStarter/routes/blogRoutes.js
+++ 02_AdvancedNodeStarter/routes/blogRoutes.js
@@ -14,9 +14,29 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
 
+    const redis = require('redis');
+    const util = require('util');
+    const redisUrl = "redis://127.0.0.1:6379";
+    const client = redis.createClient(redisUrl);
+    client.get = util.promisify(client.get)
+    
+    // do we have any data related to this query
+    const cacheBlogs = await client.get(req.user.id);
+
+    // if yes then repond to the query and get the data from cache and return
+    if(cacheBlogs){
+      console.log("SERVING FROM CACHE...")
+      return res.send(JSON.parse(cacheBlogs));
+    }
+
+    // else query from mongoDB
+
+    console.log("SERVING FROM MONGODB...")
+    const blogs = await Blog.find({ _user: req.user.id });
     res.send(blogs);
+    // save the data from mongoDB in cache
+    client.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
